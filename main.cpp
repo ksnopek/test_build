551b1492c2143b559322fa4998d8825c73ee43fe
kamil <heh@heh.com>
2016-03-26 07:56:32 +0000

    main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main()
 int b;
 std::cout << "Test" << std::endl;
 auto a =3;
+int w[10];
+w[10]=0;
 }
 
---
+++ prepere_for_push.sh
@@ -0,0 +1,19 @@
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+        against=HEAD
+else
+        # Initial commit: diff against an empty tree object
+        against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+# We should not pass non-C/C++ files to cppcheck. Filter filenames with pattern.
+pattern='\.(c|cpp|cc|cxx|h|hpp)$'
+changed_files=$(git diff-index --cached --name-only $against | grep -E $pattern)
+
+if [ -n "$changed_files" ]; then
+    cppcheck --enable=all --error-exitcode=1 $changed_files
+    exit $?
+fi
+
+echo "testaa"
+
